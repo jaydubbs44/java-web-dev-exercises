ec3c5a49cb574655b474049acbb1f0579eb63c16
Will Christen <christenwill58@gmail.com>
2022-07-21 20:23:03 -0400

    Completed a few of the bonus missions

--- src/org/launchcode/java/studios/countingcharacters/CharacterCount.java
+++ src/org/launchcode/java/studios/countingcharacters/CharacterCount.java
@@ -2,6 +2,8 @@ package org.launchcode.java.studios.countingcharacters;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
+
 public class CharacterCount {
     public static void main(String[] args) {
         String manyWords = "If the product of two terms is zero then common sense says at least " +
@@ -10,13 +12,20 @@ public class CharacterCount {
                 "allowing that side of the equation to equal zero. Once you’ve done that, " +
                 "it’s pretty straightforward from there.";
 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a character, word, phrase, sentence or paragraph you would like analyzed: ");
+
+            manyWords = input.nextLine();
+
+        input.close();
+
         String[] charactersInManyWords = manyWords.split("");
         HashMap<String, Integer> characterCounts = new HashMap<>();
         for (String item : charactersInManyWords){
-            if (!characterCounts.containsKey(item)){
-                characterCounts.put(item, 1);
-            } else if (characterCounts.containsKey(item)){
-                characterCounts.put(item, characterCounts.get(item)+1);
+            if (!characterCounts.containsKey(item.toLowerCase())){
+                characterCounts.put(item.toLowerCase(), 1);
+            } else if (characterCounts.containsKey(item.toLowerCase())){
+                characterCounts.put(item.toLowerCase(), characterCounts.get(item.toLowerCase())+1);
             }
         }
       for (Map.Entry<String, Integer> character : characterCounts.entrySet()){
